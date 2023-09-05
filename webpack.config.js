1495a687b2a04d69ad509facc00cbb151c19c8e7
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-09-05 06:09:08 +0300

    feat: add image-webpack-loader for project images

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,70 @@ module.exports = {
   ],
   module: {
     rules: [
-
+      {
+        test: /\.svg$/, //TODO check the .ico files!
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/svg/[name][ext]',
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/img/[name][ext]',
+        }
+      },
     ]
   }
 }
